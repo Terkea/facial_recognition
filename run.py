6a8f899457f24651fd087e307c479d890e12fb5c
Terkea <terkeabt@gmail.com>
2020-02-19 15:26:52 +0000

    load the images as nparrays

--- run.py
+++ run.py
@@ -1,26 +1,40 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # # TensorFlow and tf.keras
-# import tensorflow as tf
-# from tensorflow import keras
+import tensorflow as tf
+from tensorflow import keras
 
 # Helper libraries
 import os
 import cv2
+from PIL import Image
+import pandas as pd
 from pandas import DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import image
 
-DATADIR = "dataset"
+PATH = os.getcwd()
+DATADIR = PATH + "/dataset"
+BATCH = PATH + "/batch.csv"
 
 # indexing the pictures by their label
 def create_cvs():
-    DIRDATA = 'dataset'
     files = [file for file in os.listdir(DATADIR)]
-    person = [file[4:7] for file in files]
+    person = [file[5:7] for file in files]
 
     dataset = {'file': files,
             'person': person
             }
     df = DataFrame(dataset, columns=['file', 'person'])
-    export_csv = df.to_csv('labeled_data.csv', sep=',', encoding='utf-8', index=False)
+    export_csv = df.to_csv('batch.csv', sep=',', encoding='utf-8', index=False)
+
+# loading the data for processing
+df = pd.read_csv(BATCH)
+
+train_images = [cv2.imread(DATADIR + f"/{image}", cv2.IMREAD_GRAYSCALE) for image in df['file']]
+train_images = np.asarray(train_images)
+
+train_labels = df['person']
+
+
