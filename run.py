e1db9b23d2d6bf5f2a0a1b8b2330e7a540ceb8d1
Terkea <terkeabt@gmail.com>
2020-02-20 03:14:57 +0000

    model architecture + reevaluation methods

--- run.py
+++ run.py
@@ -3,20 +3,23 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
+tf.get_logger().setLevel('INFO')
 
 # Helper libraries
 import os
 import cv2
 import shutil  
-from PIL import Image
-import pandas as pd
-from pandas import DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import image
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 
+
+PATH = os.getcwd()
+DATASET = os.path.join(PATH, "dataset")
+CATEGORIES = [folder for folder in os.listdir(DATASET)]
+INDEX_VALUES = [i for i in range(30)]
+
+# Creates a folder for each individual's pictures
 def normalize_dataset():
     for file in os.listdir("dataset"):
         try:
@@ -27,17 +30,10 @@ def normalize_dataset():
 
         shutil.move(os.getcwd() + f"/dataset/{file}", os.getcwd() + f"/dataset/{file[5:7]}/{file}")
 
-PATH = os.getcwd()
-DATASET = os.path.join(PATH, "dataset")
-CATEGORIES = [folder for folder in os.listdir(DATASET)]
-INDEX_VALUES = [i for i in range(30)]
-
-# normalize_dataset()
-
+# Generate the trainig data (image, label)
 def create_training_data():
     _images = []
     _labels = []
-
     for category in CATEGORIES:
         class_num = CATEGORIES.index(category)
         new_path = os.path.join(DATASET, category)
@@ -47,10 +43,6 @@ def create_training_data():
             _labels.append(class_num)
     return (_images, _labels)
 
-(training_images, training_labels) = create_training_data()
-
-training_images = np.array(training_images)
-training_images = training_images / 255.0
 
 def create_model():
     model = keras.Sequential([
@@ -66,11 +58,12 @@ def create_model():
 
     return model
 
+# Create a callback that saves the model's weights
 def train_model(model):
     checkpoint_path = "model/model.ckpt"
     checkpoint_dir = os.getcwd()
 
-    # Create a callback that saves the model's weights
+    
     cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                     save_weights_only=True,
                                                     verbose=1)
@@ -82,10 +75,40 @@ def train_model(model):
             validation_data=(np.array(training_images),np.array(training_labels)),
             callbacks=[cp_callback])  # Pass callback to training
 
-train_model(create_model())
-
-# Create a basic model instance
-model = create_model()
+    return model
 
 # Display the model's architecture
-model.summary()
+def model_architecture():
+    model = train_model(create_model())
+    model.summary()
+
+def reevaluate_model():
+    checkpoint_path = "model/model.ckpt"
+    # Create a basic model instance
+    model = create_model()
+
+    # Evaluate the model
+    loss, acc = model.evaluate(x=np.array(training_images),  y=np.array(training_labels), verbose=2)
+    print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
+
+    # Loads the weights
+    model.load_weights(checkpoint_path)
+
+    # Re-evaluate the model
+    loss,acc = model.evaluate(x=np.array(training_images),  y=np.array(training_labels), verbose=2)
+    print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
+    
+
+if __name__ == "__main__":
+    (training_images, training_labels) = create_training_data()
+    training_images = np.array(training_images)
+    training_images = training_images / 255.0
+
+    # create and train the model
+    train_model(create_model())
+
+    # info about the architecture
+    model_architecture()
+
+    # reevaluate the saved model
+    reevaluate_model()
