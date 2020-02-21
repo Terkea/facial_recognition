e69c8aa96af8e42144a0596630bbdf8f04e97edd
Terkea <terkeabt@gmail.com>
2020-02-21 20:20:06 +0000

    plots

--- run.py
+++ run.py
@@ -105,6 +105,37 @@ def reevaluate_model():
     print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
     
 
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format([predicted_label],
+                                100*np.max(predictions_array),
+                                [true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array, true_label[i]
+  plt.grid(False)
+  plt.xticks(range(30))
+  plt.yticks([])
+  thisplot = plt.bar(range(30), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
 if __name__ == "__main__":
     (training_images, training_labels) = create_training_data()
     training_images = np.array(training_images)
@@ -112,8 +143,25 @@ if __name__ == "__main__":
 
     # visualize_random_pictures()
 
-    print("create and train the model \n")
-    train_model(create_model())
+    # print("create and train the model \n")
+    # train_model(create_model())
+
+    probability_model = tf.keras.Sequential([train_model(create_model()), 
+                                         tf.keras.layers.Softmax()])
+    
+    predictions = probability_model.predict(np.array(training_images))
+
+    num_rows = 5
+    num_cols = 10
+    num_images = num_rows*num_cols
+    plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2*num_cols, 2*i+1)
+        plot_image(1, predictions[1], training_labels, np.array(training_images))
+        plt.subplot(num_rows, 2*num_cols, 2*i+2)
+        plot_value_array(1, predictions[1], training_labels)
+    plt.tight_layout()
+    plt.show()
 
     # print("model architecture \n")
     # model_architecture()
